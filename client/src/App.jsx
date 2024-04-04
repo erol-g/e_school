3e8459838c095b95232f61b9e803d4c1a3b09481
busratok <busratokuc@gmail.com>
2024-04-04 04:08:02 +0100

    change-password route is added to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css'
-import Login from './Pages/Login';
-import Home from './Pages/Home';
-import DirectorPage from './Pages/DirectorPage';
-import TeachersPage from './Pages/TeachersPage';
-import StudentsPage from './Pages/StudentsPage';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+import DirectorPage from "./Pages/DirectorPage";
+import TeachersPage from "./Pages/TeachersPage";
+import StudentsPage from "./Pages/StudentsPage";
+import ChangePassword from "./Pages/ChangePassword";
 function App() {
-
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Home/>}></Route>
-      <Route path='/login' element={<Login/>}></Route>
-      <Route path='/director-page' element={<DirectorPage/>}></Route>
-      <Route path='/teacher-page' element={<TeachersPage/>}></Route>
-      <Route path='/student-page' element={<StudentsPage/>}></Route>
-
-    </Routes>
-
-      
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/director-page" element={<DirectorPage />}></Route>
+        <Route path="/teacher-page" element={<TeachersPage />}></Route>
+        <Route path="/student-page" element={<StudentsPage />}></Route>
+        <Route path="/change-password" element={<ChangePassword />}></Route>
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
