187b4c7dabeeaca2aff87359c4b4a29c0994c0f9
busratok <busratokuc@gmail.com>
2024-04-05 13:11:47 +0100

    navigation to change password page is added to studentPage and teacherPage

--- client/src/Pages/StudentsPage.jsx
+++ client/src/Pages/StudentsPage.jsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router";
 import PageHeader from "./HeaderPage";
 
 const StudentsPage = () => {
+  const navigate = useNavigate();
   return (
     <div className="page-container">
       <PageHeader title="Student Page" />
-      <button>to change your password</button>
+      <button onClick={() => navigate("/change-password")}>
+        to change your password
+      </button>
       <button>to see your notes</button>
       <button>to see your information</button>
       <button>to send a message</button>
